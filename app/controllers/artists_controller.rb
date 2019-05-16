7222859616de9594b724109c4ede01b1aa432397
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-16 16:28:54 -0600

    artists CRUD complete

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+  
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    if @artist.update(params.require(:artist).permit(:artist_name, :genre))
+      redirect_to artists_path(@artist.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(params.require(:artist).permit(:artist_name, :genre))
+      redirect_to artists_path(@artist.id)
+    else
+      render :edit
+    end
+  end
+
+end
