05c6ee3f956a1c5fd0b0c230a13c6e1c42356f1d
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-20 13:46:15 -0600

    fixed broken db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,17 @@
 # ]
 # "Godsmack", "Disturbed", "Metallica", "Garth Brooks", "John Michael Montgomery", "Led Zeppelin", "Doors"
 
-songs = ["Lose Yourself↵", "Mockingbird↵", "8 Mile↵", "Cleanin' Out My Closet↵", "Stan↵", "Loose Yourself↵", "8 Mile Freestyle↵", "313↵", "Sweet Home Alabama↵", "Soldier↵", "Halie's Song↵", "Remember Me↵", "Till I Collapse↵", "Backstabber↵", "8 Mile Road↵", "Without Me Dirty Version↵", "97 Bonnie And Clyde↵", "Criminal↵", "8 Mile Freestyle Pt.ii Vs Lotto↵", "The Real Slim Shady↵", "8 Mile Freestyle Pt.iii Vs Papa Doc↵", "Without Me Clean Version↵", "Ass Like That↵", "My Name Is... (bootleg Version)↵", "Marshall Mathers↵", "Any Man↵", "Doe Rae Me↵", "Rap Superstar↵", "One Shot 2 Shot↵", "Never Enough↵", "Kim↵", "Back Stabber↵", "313 — Infinite↵", "Role Model↵", "3 Verses↵", "Brain Damage↵", "Business↵", "Crazy In Love↵", "B—rabbit Vs. Marv One↵", "Bad Influence↵", "Encore Curtains Down↵", "I'm Shady↵", "My Name Is...↵", "Just The Two Of Us↵", "B—rabbit Vs. Diffusion↵", "We As Americans↵", "Dance Nation — Words↵", "Bully↵", "Curtains Up (encore Version)↵", "Dads Gone Crazy↵", "Big Weenie↵", "A Few Different Frestyles↵", "8 Mile Freestyle Vs Lickety Split↵", "B—rabbit On The Roof Top↵", "As The World Turns↵", "Dead Wrong (remix)↵", "Bad Guys Always Die↵", "Bad Meets Evil↵", "Come On In↵", "Baby Come Back↵", "Before Your Love↵", "Sing For The Moment↵", "Bump Heads↵", "Comin' Out My Closet (parody)↵", "Doe Ray Me↵", "Curtains Close↵", "Curtains Close (skit)↵", "Public Service Announcement↵", "Amityville↵", "Dead Wrong↵", "Addicted↵", "For You Only↵", "Rock Bottom↵", "Square Dance↵", "Sometimes↵", "Spend Some Time↵", "Public Service Announcement 2000↵", "Searchin'↵", "Rabbit In Parking Lot Outside The Chin Tiki↵", "Rabbit Run↵"
+songs = [
+  "Float On↵", "Breakthrough↵", "Bukowski↵", "Every Penny Fed â€˜ar↵", "Here It Comes↵", "Humpin' Pumpklin↵", "I Came As A Rat (long Walk Off A Short Dock)↵", "Dance Hall↵", "Cowboy Dan↵", "Convenient Parking↵", "Gravity Rides Everything↵", "5—4—3—2—1... Lips Off↵", "Dirty Fingernails↵", "Custom Concern↵", "Broke↵", "Interstate 8↵", "Karma's Payment↵", "Follow Through↵", "Jesus Christ Was An Only Child↵", "Grey Ice Water↵", "Dark Center Of The Universe↵", "I Came As A Rat↵", "Life Like Weeds↵", "Edit The Sad Parts↵", "Birds Vs. Worms↵", "A Life Of Arctic Sounds↵", "Bury Me With It↵", "Beta Carotene↵", "Baby Blue Sedan↵", "Heart Cooks Brain↵", "Black Cadillacs↵", "A Manic Depressive Named Laughing Boy↵", "A Different City↵", "Dramamine↵", "Alone Down There↵", "It Always Rains On A Picnic↵", "3 Inch Horses, Two Faced Monsters↵", "Four Fingered Fisherman↵", "Exit Does Not Exist↵", "Austral Opithecus↵", "Beach Side Property↵", "All Nite Diner↵", "Blame It On The Tetons↵", "Bankrupt On Selling↵", "Head South↵", "Classy Plastic Lumber↵", "Dog Paddle↵", "Dukes Up↵", "From Point A To Point B↵", "The Fruit That Ate Itself↵", "The Parasites Will Be Excited When You're Dead↵", "The Stars Are Projectors↵", "The View↵", "The Waydown↵", "Think Long↵", "The Good Times Are Killing Me↵", "The World At Large↵", "The Devils Workday↵", "The Cold Part↵", "Teeth Like God's Shoeshine↵", "Swy↵", "Sweater↵", "Sunspots In The House Of The Late Scapegoat↵", "Third Planet↵", "This Devil's Workday↵", "Workin' On Leavin' The Livin'↵", "Willful Suspension Of Disbelief↵", "Wild Pack Of Family Dogs↵", "Whenever You See Fit↵", "Whenever You Breathe Out, I Breathe In (positive|negative)↵", "What People Are Made Of↵", "Wagon Ride Return↵", "Ugly Cassanova↵", "Tundra|desert↵", "Trucker's Atlas↵", "Trailer Trash↵", "Too Many Fiestas For Reuben↵", "Tiny Cities Made Of Ashes↵", "You're The Good Things↵", "Summer↵"
 ]
 
-x = 13
+x = 5
 songs.each do |song|
   song.sub!(/(↵)/,'')
   Song.create(
     song_name: song,
     artist_id: x,
-    board_id: 6,
+    board_id: nil,
   )
 end
 
